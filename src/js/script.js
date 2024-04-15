f12cf37d002f06fb0475201635e944044fac5a14
Arsen <arisov159@mail.ru>
2024-04-15 09:59:47 +0500

    slider rebuilt, add new feautures

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,33 @@
-const sliderContainer = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slider__item');
+//Slider
+const slider = document.querySelector('.slider');
+const sliderContainer = document.querySelector('.slider__wrapper');
+const slides = document.querySelectorAll('.slider__item--size--l');
+
+const windowWidth = document.documentElement.clientWidth;
+slider.style.width = `${windowWidth}px`;
+sliderContainer.style.width = `${100 * slides.length}%`;
+slides.forEach((el) => (el.style.flexBasis = `${windowWidth}px`));
 
 let slideCounter = 0;
 
-const moveSlides = function (count) {
-  slides.forEach(
-    (el, i) => (el.style.transform = `translateX(${(i - count) * 100}%)`)
-  );
+const moveSlides = function (slide) {
+  sliderContainer.style.transform = `translateX(${-slide * windowWidth}px)`;
 };
 
 moveSlides(slideCounter);
 
-sliderContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('slider__left')) {
+slider.addEventListener('click', function (e) {
+  if (e.target.classList.contains('slider__right')) {
     slideCounter +=
       slideCounter >= slides.length - 1 ? -(slides.length - 1) : 1;
   }
-  if (e.target.classList.contains('slider__right')) {
+  if (e.target.classList.contains('slider__left')) {
     slideCounter += slideCounter ? -1 : slides.length - 1;
   }
   moveSlides(slideCounter);
 });
+
+setInterval(function () {
+  slideCounter += slideCounter >= slides.length - 1 ? -(slides.length - 1) : 1;
+  moveSlides(slideCounter);
+}, 3500);

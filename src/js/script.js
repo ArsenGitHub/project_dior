145c93cb5fcbd092331b5147a43a7d0dac58ac46
Arsen <arisov159@mail.ru>
2024-04-16 10:37:42 +0500

    add descrioption section, some slider fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,66 @@
-//Slider
-const slider = document.querySelector('.slider');
-const sliderContainer = document.querySelector('.slider__wrapper');
-const slides = document.querySelectorAll('.slider__item--size--l');
+//Large Slider
+const sliderLarge = document.querySelector('.slider--size--l');
+const sliderLargeWrapper = document.querySelector('.slider__wrapper--size--l');
+const slidesLarge = document.querySelectorAll('.slider__item--size--l');
 
-const windowWidth = document.documentElement.clientWidth;
-slider.style.width = `${windowWidth}px`;
-sliderContainer.style.width = `${100 * slides.length}%`;
-slides.forEach((el) => (el.style.flexBasis = `${windowWidth}px`));
+let windowWidth = document.documentElement.clientWidth;
+sliderLarge.style.width = `${windowWidth}px`;
+sliderLargeWrapper.style.width = `${100 * slidesLarge.length}%`;
+slidesLarge.forEach((el) => (el.style.flexBasis = `${windowWidth}px`));
 
 let slideCounter = 0;
 
 const moveSlides = function (slide) {
-  sliderContainer.style.transform = `translateX(${-slide * windowWidth}px)`;
+  sliderLargeWrapper.style.transform = `translateX(${-slide * windowWidth}px)`;
 };
 
 moveSlides(slideCounter);
 
-slider.addEventListener('click', function (e) {
+sliderLarge.addEventListener('click', function (e) {
   if (e.target.classList.contains('slider__right')) {
     slideCounter +=
-      slideCounter >= slides.length - 1 ? -(slides.length - 1) : 1;
+      slideCounter >= slidesLarge.length - 1 ? -(slidesLarge.length - 1) : 1;
   }
   if (e.target.classList.contains('slider__left')) {
-    slideCounter += slideCounter ? -1 : slides.length - 1;
+    slideCounter += slideCounter ? -1 : slidesLarge.length - 1;
   }
   moveSlides(slideCounter);
 });
 
 setInterval(function () {
-  slideCounter += slideCounter >= slides.length - 1 ? -(slides.length - 1) : 1;
+  slideCounter +=
+    slideCounter >= slidesLarge.length - 1 ? -(slidesLarge.length - 1) : 1;
   moveSlides(slideCounter);
 }, 3500);
+
+//Small Slider
+const sliderSmall = document.querySelector('.slider--size--s');
+const sliderSmallWrapper = document.querySelector('.slider__wrapper--size--s');
+const slidesSmall = document.querySelectorAll('.slider__item--size--s');
+const sliderSmallImg = document.querySelector('.slider__img--size--s');
+const imgWidth = sliderSmallImg.naturalWidth;
+const imgGap = parseFloat(
+  window.getComputedStyle(sliderSmallWrapper).columnGap
+);
+
+const move = function (slide) {
+  sliderLargeWrapper.style.transform = `translateX(${-slide * windowWidth}px)`;
+};
+
+// sliderSmall.style.width = `${imgWidth}px`;
+sliderSmallWrapper.style.width = `${
+  imgWidth * slidesSmall.length + imgGap * (slidesSmall.length - 1)
+}px`;
+// slidesSmall.forEach((el) => (el.style.flexBasis = `${windowWidth}px`));
+sliderSmall.addEventListener('click', function (e) {
+  if (e.target.classList.contains('slider__right')) {
+    slideCounter +=
+      slideCounter >= slidesLarge.length - 1 ? -(slidesLarge.length - 1) : 1;
+  }
+  if (e.target.classList.contains('slider__left')) {
+    slideCounter += slideCounter ? -1 : slidesLarge.length - 1;
+  }
+});
+
+// Первым элемент смещается и потом удаляется
+// В это время он вставляется в конец
